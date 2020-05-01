1b925585d2d521f86c20a1957b0cc28f754fe66e
David Ochsner <ochsnerd@student.ethz.ch>
2020-05-01 18:11:18 +0200

    Remove unused potential-class in script

--- report/scripts/analytic.py
+++ report/scripts/analytic.py
@@ -1,8 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from scipy.stats import norm
-
 from ip_mcmc import (MCMCSampler,
                      AnalyticAccepter, CountedAccepter, StandardRWAccepter, pCNAccepter,
                      StandardRWProposer, pCNProposer,
@@ -30,22 +28,6 @@ def normal(x):
     return np.exp(-0.5*x**2) / np.sqrt(2 * np.pi)
 
 
-class Potential():
-    """An artificial potential allowing sampling from
-    a analytical density using Bayes-MCMC.
-
-    Assuming a gaussian prior (scipy.stats.norm)
-    """
-    def __init__(self, rho, prior):
-        self.rho = rho
-        self.prior = prior
-
-    def __call__(self, x):
-        # scipy.stats.norm.logpdf includes the normalisation, but that can be ignored
-        # as only potential differnces matter
-        return -np.log(self.rho(x)) + self.prior.logpdf(x)
-
-
 def create_StandardRWSampler(density):
     mean = np.array([0])
     covariance = np.array([1], ndmin=2)
@@ -65,7 +47,7 @@ def create_pCNSampler(density):
     mean = np.array([0])
     covariance = np.array([1], ndmin=2)
     prior = GaussianDistribution(mean, covariance)
-    
+
     potential = AnalyticPotential(posterior=density, prior=prior)
 
     # beta != delta of other proposers, but
@@ -116,12 +98,14 @@ def plot_sampler_characteristics(sampler_generator, density, name):
     plt.title("Autocorrelation of sampling from " + name + " density")
     store_figure(name + "_ac")
 
+    print(name + " done")
+
 
 def plot_autocorrelations(density, sampler_generators, names):
     for sampler_generator, name in zip(sampler_generators, names):
         create_autocorrelation_plot(sampler_generator(density), name)
 
-        print(name + "done")
+        print(name + " done")
 
     plt.title("Autocorrelation")
     plt.legend()
@@ -140,7 +124,7 @@ def main():
 
     # plot_sampler_characteristics(create_StandardRWSampler, normal, "standard_normal")
 
-    # plot_sampler_characteristics(create_pCNSampler, bimodal_density, "pCN_bimodal")
+    plot_sampler_characteristics(create_pCNSampler, bimodal_density, "pCN_bimodal")
 
     # plot_sampler_characteristics(create_pCNSampler, normal, "pCN_normal")
 
