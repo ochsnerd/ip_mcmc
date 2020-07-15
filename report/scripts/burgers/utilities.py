f69d37bce67bc1ffe9fbc2f045ad798e33de7a5b
David Ochsner <ochsnerd@student.ethz.ch>
2020-07-15 11:50:33 +0200

    Move Wasserstein distance from burgers to general

--- report/scripts/burgers/utilities.py
+++ report/scripts/burgers/utilities.py
@@ -1,5 +1,3 @@
-import ot  # Wasserstein distance
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -7,9 +5,13 @@ from scipy.stats import norm
 
 import sys
 sys.path.append("/home/david/fs20/thesis/code/report/scripts/")
+from helpers import (store_figure,
+                     load_or_compute,
+                     autocorrelation,
+                     wasserstein_distance)
+
 sys.path.append("/home/david/fs20/thesis/code/report/scripts/burgers")
 from rusanov import RusanovFVM
-from helpers import store_figure, load_or_compute, autocorrelation
 
 
 class FVMObservationOperator:
@@ -191,75 +193,6 @@ def clean_samples(x):
     x_ = x_[:, ::uncorrelated_sample_spacing(x_)]
 
     return x_
-
-
-def wasserstein_distance(d1, d2, intervals):
-    """Compute the Wasserstein-distance between d1, d2
-
-    d1 and d2 are densities (assumed to be normalized)
-    supported on the given intervals
-    """
-    assert d1.shape == d2.shape, ""
-    assert len(d1.shape) == intervals.shape[0], ""
-    assert intervals.shape[1] == 2, ""
-
-    # create metric
-    nd = len(d1.shape)
-    ax_ticks = np.meshgrid(*[np.linspace(start=intervals[i, 0],
-                                         stop=intervals[i, 1],
-                                         num=d1.shape[i])
-                             for i in range(nd)], indexing='ij')
-
-    grid = np.stack([ticks.flatten() for ticks in ax_ticks], axis=1)
-    M = ot.dist(grid, grid, metric='euclidean')
-
-    return ot.emd2(d1.flatten(), d2.flatten(), M)
-
-
-def test_wasserstein_distance():
-    # two 1D histograms with 5 bins
-    n = 5
-    x_1 = np.arange(n).reshape((n, 1))
-    M1 = ot.dist(x_1, x_1, metric='euclidean')
-
-    # two delta peaks
-    d_11 = [1,0,0,0,0]
-    d_12 = [0,0,1,0,0]
-    assert ot.emd2(d_11, d_12, M1) == 2, ""
-
-    # two 2D histograms
-    n1, n2 = (5, 8)
-    xv, yv = np.meshgrid(np.arange(n1), np.arange(n2), indexing='ij')
-    grid = np.stack((xv.flatten(), yv.flatten()), axis=1)
-    M = ot.dist(grid, grid, metric='euclidean')
-
-    # from all corners to bottom left
-    d_21 = np.zeros((n1, n2))
-    d_21[0,0] = .25
-    d_21[0, -1] = .25
-    d_21[-1, 0] = .25
-    d_21[-1, -1] = .25
-    d_22 = np.zeros((n1, n2))
-    d_22[0, -1] = 1
-
-    assert np.isclose(ot.emd2(d_21.flatten(), d_22.flatten(), M),
-                      ((n1-1) + (n2-1) + np.sqrt((n1-1)**2 + (n2-1)**2))/4), ""
-
-    # using my function
-    n1, n2, n3 = (3, 4, 6)
-    intervals = np.array([[-1, 1],  # nice integer spacing
-                          [0, 3],
-                          [-5, 0]])
-
-    d_31 = np.zeros((n1, n2, n3))
-    d_31[0,0,0] = 1
-    d_32 = np.zeros((n1, n2, n3))
-    d_32[2,2,2] = 1
-
-    assert np.isclose(wasserstein_distance(d_31, d_32, intervals), np.sqrt(12)), ""
-
-
-test_wasserstein_distance()
 
 
 def show_chain(chain, burn_in, sample_interval):
--- report/scripts/helpers.py
+++ report/scripts/helpers.py
@@ -1,3 +1,5 @@
+import ot  # Wasserstein distance
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -50,3 +52,72 @@ def autocorrelation(samples, tau_max):
     ac /= avg_over
 
     return ac
+
+
+def wasserstein_distance(d1, d2, intervals):
+    """Compute the Wasserstein-distance between d1, d2
+
+    d1 and d2 are densities (assumed to be normalized)
+    supported on the given intervals
+    """
+    assert d1.shape == d2.shape, ""
+    assert len(d1.shape) == intervals.shape[0], ""
+    assert intervals.shape[1] == 2, ""
+
+    # create metric
+    nd = len(d1.shape)
+    ax_ticks = np.meshgrid(*[np.linspace(start=intervals[i, 0],
+                                         stop=intervals[i, 1],
+                                         num=d1.shape[i])
+                             for i in range(nd)], indexing='ij')
+
+    grid = np.stack([ticks.flatten() for ticks in ax_ticks], axis=1)
+    M = ot.dist(grid, grid, metric='euclidean')
+
+    return ot.emd2(d1.flatten(), d2.flatten(), M)
+
+
+def test_wasserstein_distance():
+    # two 1D histograms with 5 bins
+    n = 5
+    x_1 = np.arange(n).reshape((n, 1))
+    M1 = ot.dist(x_1, x_1, metric='euclidean')
+
+    # two delta peaks
+    d_11 = [1,0,0,0,0]
+    d_12 = [0,0,1,0,0]
+    assert ot.emd2(d_11, d_12, M1) == 2, ""
+
+    # two 2D histograms
+    n1, n2 = (5, 8)
+    xv, yv = np.meshgrid(np.arange(n1), np.arange(n2), indexing='ij')
+    grid = np.stack((xv.flatten(), yv.flatten()), axis=1)
+    M = ot.dist(grid, grid, metric='euclidean')
+
+    # from all corners to bottom left
+    d_21 = np.zeros((n1, n2))
+    d_21[0,0] = .25
+    d_21[0, -1] = .25
+    d_21[-1, 0] = .25
+    d_21[-1, -1] = .25
+    d_22 = np.zeros((n1, n2))
+    d_22[0, -1] = 1
+
+    assert np.isclose(ot.emd2(d_21.flatten(), d_22.flatten(), M),
+                      ((n1-1) + (n2-1) + np.sqrt((n1-1)**2 + (n2-1)**2))/4), ""
+
+    # using my function
+    n1, n2, n3 = (3, 4, 6)
+    intervals = np.array([[-1, 1],  # nice integer spacing
+                          [0, 3],
+                          [-5, 0]])
+
+    d_31 = np.zeros((n1, n2, n3))
+    d_31[0,0,0] = 1
+    d_32 = np.zeros((n1, n2, n3))
+    d_32[2,2,2] = 1
+
+    assert np.isclose(wasserstein_distance(d_31, d_32, intervals), np.sqrt(12)), ""
+
+
+test_wasserstein_distance()
